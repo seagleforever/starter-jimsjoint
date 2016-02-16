3aa81f71c0e3ca11dbb99ae2ba028747c41f7572
Siwon <susimon@live.com>
2016-02-16 11:46:41 -0800

    #4 handle order display fix

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -37,6 +37,8 @@ class Order extends Application {
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
         //FIXME
+        $this->data['title'] = 'Order # '.$order_num .
+                ' (' . number_format($this->Orders->total($order_num), 2) . ')';
 
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
@@ -71,7 +73,7 @@ class Order extends Application {
     // make a menu ordering column
     function make_column($category) {
         //FIXME
-        return $items;
+        return $this->Menu->some('category', $category);
     }
 
     // add an item to an order
